474c3080663ccbc1a92090fcd8fbeaf3c93b6ba7
Jhonatan Hulse <jhonatan.hulse@gmail.com>
2016-11-21 21:29:44 -0200

    Add class responsible for application initialization and gradle script to build jar file

---
+++ src/main/java/kartrank/KartRankInitializer.java
@@ -0,0 +1,11 @@
+package kartrank;
+
+/**
+ * Class responsible for initializing the kart rank application
+ */
+public class KartRankInitializer {
+
+    public static void main(String[] args) {
+        System.out.println("KartRankInitializer");
+    }
+}
